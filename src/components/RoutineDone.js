19157f852c8079a8ec08ed7f63a95cf6cb90f349
Zeyad Khattab <41998602+ZeyadKhattab@users.noreply.github.com>
2020-05-10 14:22:19 +0200

    Added a simple graph with dummy data

--- src/components/RoutineDone.js
+++ src/components/RoutineDone.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { markRoutineAsDone } from "../backend/routes";
 import App from "../App";
+import Graph from "./Graph";
 export default class RoutineDone extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
@@ -22,6 +23,7 @@ export default class RoutineDone extends React.Component {
           <input type="text" placeholder="MinutesNeeded" name="minutes"></input>
           <input type="submit" onClick={this.handleSubmit.bind(this)}></input>
         </form>
+        <Graph></Graph>
       </div>
     );
   }
