113c332580b9069fc25363ac74961fc79ee668be
Zeyad Khattab <41998602+ZeyadKhattab@users.noreply.github.com>
2020-05-15 02:27:12 +0200

    completed Modal issue

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,76 @@
 import React, { useState } from "react";
 import RoutineCard from "./RoutineCard";
-import { getTodaysRoutines, markRoutineAsDone } from "../backend/routes";
+import {
+  getTodaysRoutines,
+  markRoutineAsDone,
+  addRoutine,
+  getRoutines,
+} from "../backend/routes";
 import { getMonth, getDayOfMonth } from "../backend/helpers";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 export default class Todos extends React.Component {
   state = {
     routines: [],
     showModal: false,
+  };
+  Example = () => {
+    const handleClose = (e) => {
+      const form = e.target.parentElement;
+      const time = form.time.value;
+      const comment = form.comment.value;
+      markRoutineAsDone(this.state.routineDoneName, comment, time);
+      this.componentDidMount();
+    };
+    return (
+      <>
+        <Modal
+          show={this.state.showModal}
+          onHide={handleClose}
+          animation={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="time">
+                <Form.Label>Time</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Time in Minutes"
+                  name="time"
+                />
+              </Form.Group>
+
+              <Form.Group controlId="comment">
+                <Form.Label>Comment</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter A comment"
+                  name="comment"
+                />
+              </Form.Group>
+              <Button variant="primary" onClick={handleClose}>
+                Mark as Done
+              </Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={() =>
+                this.setState({ showModal: false, routineDoneName: "" })
+              }
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
   };
   componentDidMount() {
     const month = getMonth();
@@ -36,12 +99,7 @@ export default class Todos extends React.Component {
             />
           ))}
         </div>
-        {this.state.showModal && (
-          <Example
-            routineName={this.state.routineDoneName}
-            // render={this.componentDidMount.bind(<Todos></Todos>)}
-          />
-        )}
+        {this.state.showModal && this.Example()}
       </React.Fragment>
     );
   }
@@ -50,30 +108,3 @@ const flexContainerStyle = {
   display: "flex",
   flexWrap: "wrap",
 };
-
-function Example(props) {
-  const [show, setShow] = useState(true);
-
-  const handleClose = () => {
-    setShow(false);
-    // console.log(props.render());
-  };
-  return (
-    <>
-      <Modal show={show} onHide={handleClose} animation={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Mark as Done
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
