b74df1c287e0a4165177aa85893480a07cd51e6f
Zeyad Khattab <41998602+ZeyadKhattab@users.noreply.github.com>
2020-05-27 19:30:50 +0200

    Updates

---
+++ precache-manifest.d2c2e7ef29034d5e732c14a53ab1d570.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003174a6e817fc832d16a4d89e2311cd",
+    "url": "/logger/index.html"
+  },
+  {
+    "revision": "3e4a6b9251bca028592b",
+    "url": "/logger/static/css/2.60d9b82a.chunk.css"
+  },
+  {
+    "revision": "a88ba58e586e6e5e2f24",
+    "url": "/logger/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3e4a6b9251bca028592b",
+    "url": "/logger/static/js/2.d4f514b8.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/logger/static/js/2.d4f514b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88ba58e586e6e5e2f24",
+    "url": "/logger/static/js/main.320b31c9.chunk.js"
+  },
+  {
+    "revision": "f0e81a613fab8931e89a",
+    "url": "/logger/static/js/runtime-main.a355f7e0.js"
+  }
+]);
