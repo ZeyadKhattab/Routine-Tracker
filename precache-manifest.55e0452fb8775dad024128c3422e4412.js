77a549f32efc098a34e7607cbcebcec9b643fcd9
Zeyad Khattab <41998602+ZeyadKhattab@users.noreply.github.com>
2020-08-22 16:52:33 +0200

    Updates

---
+++ precache-manifest.55e0452fb8775dad024128c3422e4412.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1017b2d7bf36bf927f1e42f2ba34536",
+    "url": "/Routine-Tracker/index.html"
+  },
+  {
+    "revision": "96954138ff873b0edc52",
+    "url": "/Routine-Tracker/static/css/2.60d9b82a.chunk.css"
+  },
+  {
+    "revision": "b8c3b4e63f5383f27cb5",
+    "url": "/Routine-Tracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "96954138ff873b0edc52",
+    "url": "/Routine-Tracker/static/js/2.f0c434dc.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Routine-Tracker/static/js/2.f0c434dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c3b4e63f5383f27cb5",
+    "url": "/Routine-Tracker/static/js/main.cbcdeed4.chunk.js"
+  },
+  {
+    "revision": "c5ef3700e07c159301e5",
+    "url": "/Routine-Tracker/static/js/runtime-main.3e8feb66.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/routine-tracker/precache-manifest.e63235f8b265984c466aae3db4ff5b76.js"
+  "/Routine-Tracker/precache-manifest.55e0452fb8775dad024128c3422e4412.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/routine-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Routine-Tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
